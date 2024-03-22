4bdbaf7c35e0fe1da0e29ef2028e7853b1c6fcc6
Mohit Varma <mv501049@gmail.com>
2024-03-22 22:53:27 +0530

    fragment learning.

--- app/src/main/java/com/example/latestcomponentpractice/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/latestcomponentpractice/fragments/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import com.example.latestcomponentpractice.R
 
 class HomeFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
