0197818526ecea8b12ccab05bb82dd3e9ad79534
Mohit Varma <mv501049@gmail.com>
2024-04-04 23:59:40 +0530

    Update CustomBindingAdapters.kt

--- app/src/main/java/com/example/latestcomponentpractice/sqrt_app/CustomBindingAdapters.kt
+++ app/src/main/java/com/example/latestcomponentpractice/sqrt_app/CustomBindingAdapters.kt
@@ -3,7 +3,6 @@ package com.example.latestcomponentpractice.sqrt_app
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 
-
 @BindingAdapter("setBackgroundColor")
 fun ConstraintLayout.setBackgroundColor(color : Int) {
     this.setBackgroundColor(color)
