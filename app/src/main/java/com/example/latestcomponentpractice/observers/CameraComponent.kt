f05e59befc1dc1a433ac0b5377ad8f8d834c08dd
Mohit Varma <mv501049@gmail.com>
2024-04-12 22:27:24 +0530

    task and back stack

---
+++ app/src/main/java/com/example/latestcomponentpractice/observers/CameraComponent.kt
@@ -0,0 +1,10 @@
+package com.example.latestcomponentpractice.observers
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class CameraComponent : LifecycleObserver {
+
+
+}
---
+++ app/src/main/java/com/example/latestcomponentpractice/task_back_stack/TaskBackStack.kt
@@ -0,0 +1,21 @@
+package com.example.latestcomponentpractice.task_back_stack
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.latestcomponentpractice.R
+
+class TaskBackStack : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_task_back_stack)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
