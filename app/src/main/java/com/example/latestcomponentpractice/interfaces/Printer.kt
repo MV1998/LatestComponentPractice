85301bf27e34f957b947293c689e7ce68919ce37
Mohit Varma <mv501049@gmail.com>
2024-04-18 23:33:59 +0530

    interfaces

--- app/src/main/java/com/example/latestcomponentpractice/interfaces/Printer.kt
+++ app/src/main/java/com/example/latestcomponentpractice/interfaces/Printer.kt
@@ -2,12 +2,25 @@ package com.example.latestcomponentpractice.interfaces
 
 import android.util.Log
 
-abstract class Printer(open val paperSize: Int) {
+/*
+screen  - resolution, height, width, pixel
+battery, company,
+ */
 
-    open fun print() {}
+interface S {
+    fun SS()
 }
 
+abstract class Resolution {
+}
 
+abstract class Mobile {
+    val resolution: Resolution? = null
+}
+
+abstract class Printer(open val paperSize: Int) {
+    open fun print() {}
+}
 class LGPrinter(override val paperSize: Int) : Printer(paperSize) {
     override fun print() {
         println("${javaClass.simpleName} print: $paperSize")
@@ -33,4 +46,22 @@ fun main() {
     PrinterManager(CanonPrinter(232)).apply {
         print()
     }
+
+    val s = Singleton()
+    val ss = Singleton()
+    println("${s.hashCode()}  and ${ss.hashCode()}")
+}
+
+class Singleton constructor() {
+
+//    companion object {
+//        private var INSTANCE : Singleton? = null
+//        fun  getInstance() : Singleton {
+//            if (INSTANCE == null) {
+//                INSTANCE = Singleton()
+//                return INSTANCE!!
+//            }
+//            return INSTANCE!!
+//        }
+//    }
 }
